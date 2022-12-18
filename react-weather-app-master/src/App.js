18577c1902ff3cc1d44574fc97530519511760b4
Nitishnk24 <nitishnkotumuchagi@gmail.com>
2022-12-18 20:54:39 +0530

    first commit

---
+++ react-weather-app-master/src/App.js
@@ -0,0 +1,100 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import Axios from "axios";
+import CityComponent from "./modules/CityComponent";
+import WeatherComponent from "./modules/WeatherInfoComponent";
+import { BrowserRouter as Router, Link } from "react-router-dom";
+
+export const WeatherIcons = {
+  "01d": "/react-weather-app/icons/sunny.svg",
+  "01n": "/react-weather-app/icons/night.svg",
+  "02d": "/react-weather-app/icons/day.svg",
+  "02n": "/react-weather-app/icons/cloudy-night.svg",
+  "03d": "/react-weather-app/icons/cloudy.svg",
+  "03n": "/react-weather-app/icons/cloudy.svg",
+  "04d": "/react-weather-app/icons/perfect-day.svg",
+  "04n": "/react-weather-app/icons/cloudy-night.svg",
+  "09d": "/react-weather-app/icons/rain.svg",
+  "09n": "/react-weather-app/icons/rain-night.svg",
+  "10d": "/react-weather-app/icons/rain.svg",
+  "10n": "/react-weather-app/icons/rain-night.svg",
+  "11d": "/react-weather-app/icons/storm.svg",
+  "11n": "/react-weather-app/icons/storm.svg",
+};
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 380px;
+  padding: 20px 10px;
+  margin: auto;
+  border-radius: 4px;
+  box-shadow: 0 3px 6px 0 #555;
+  background: white;
+  font-family: Montserrat;
+`;
+
+const AppLabel = styled.span`
+  color: black;
+  margin: 20px auto;
+  font-size: 18px;
+  font-weight: bold;
+`;
+const CloseButton = styled.span`
+  padding: 2px 3px;
+  background-color: black;
+  border-radius: 50%;
+  color: white;
+  position: absolute;
+`;
+
+function App() {
+  const [city, updateCity] = useState();
+  const [weather, updateWeather] = useState();
+  const fetchWeather = async (e) => {
+    e.preventDefault();
+    const response = await Axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=fe4feefa8543e06d4f3c66d92c61b69c`
+    );
+    updateWeather(response.data);
+  };
+  return (
+    <Container>
+      <AppLabel>Flood Info</AppLabel>
+
+      <div
+        style={{
+          display: "flex",
+          width: "100%",
+          flexDirection: "row",
+        }}
+      >
+        <div>
+          <a href="https://sites.research.google/floods/?lat=16.130262012034756&lng=2.8125&zoom=3.6">
+            <button style={{}}>Click hear to get present flood road</button>
+          </a>
+        </div>
+        <div>
+          <a href="https://www.google.com/maps/@12.9794048,77.594624,12z">
+            <button>Check your present location</button>
+          </a>
+        </div>
+        <div>
+          <a href="https://bhuvan-app1.nrsc.gov.in/disaster/disaster.php">
+            <button>India's most flooded areas</button>
+          </a>
+        </div>
+      </div>
+      <AppLabel>Weather Info</AppLabel>
+
+      {city && weather ? (
+        <WeatherComponent weather={weather} city={city} />
+      ) : (
+        <CityComponent updateCity={updateCity} fetchWeather={fetchWeather} />
+      )}
+    </Container>
+  );
+}
+
+export default App;
